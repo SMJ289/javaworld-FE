eac948173dbf528a966bbe8ea98c9d38a30a8f24
Steven Meyers <sjjmeyers@gmail.com>
2020-11-05 15:20:55 -0600

    Add Cafe Screen

---
+++ screens/CafeScreen.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { fetchCafes } from "../ApiCalls"
+import { Text, View } from "../components/Themed"
+
+export default function Cafe() {
+ return (
+   <View>
+     <Text> Hello World! </Text>
+   </View>
+ )
+}
